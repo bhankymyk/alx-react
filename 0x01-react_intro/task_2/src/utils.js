94f23527690d8c1527b4bda559b70105819540e5
bhankymyk <bankole123michael@gmail.com>
2023-01-29 10:09:26 +0100

    task 3 done

--- 0x01-react_intro/task_2/src/utils.js
+++ 0x01-react_intro/task_2/src/utils.js
@@ -2,4 +2,4 @@ export const getFullYear = () => new Date().getFullYear();
 
 export const getFooterCopy = (isIndex) =>
               isIndex ? 'Holberton School' : 'Holberton School main dashboard';
-              
+              export const getLatestNotification = () => "<strong>Urgent requirement</strong> - complete by EOD";
