addb9a7989509d405618e3d02a49b6fcef20d812
bhankymyk <bankole123michael@gmail.com>
2023-01-29 12:15:52 +0100

    setup gh pages

---
+++ 0x01-react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,27 @@
+import logo from './holbertonLogo.jpg';
+import { getFullYear, getFooterCopy } from './utils';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+    <p>Login to access the full dashboard</p>
+      <label>Email</label>
+      <input name="email" type="email" id="email"></input>
+      <label>Password</label>
+      <input name="password" type="password" id="password"></input>
+      <button>OK</button>
+      </body>
+      <footer className="App-footer">
+      <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ 0x01-react_intro/task_4/dashboard/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Notifications from './Notifications';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+	<Notifications />
+    <App />
+  </React.StrictMode>
+);
+
+// ReactDOM.render(
+	// <React.StrictMode>
+	// {/* </React.StrictMode>, */}
+	// document.getElementById('root-notifications')
+// );
+// 
